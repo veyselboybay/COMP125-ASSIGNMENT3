c5392c81da06df95d2dfadf032f62d6db088edae
veyselboybay <veysel.boybay@outlook.com>
2020-07-03 22:50:08 -0400

    contact page modified

--- Script/app.js
+++ Script/app.js
@@ -1,9 +1,21 @@
+/*
+    AUTOR: VEYSEL BOYBAY
+    FILE: APP.JS
+    WEBSITE: https://veyselboybay.github.io/COMP125-Assignment-2/
+    DESCRIPTION: THIS FILE CONTAINS WEB PAGE JAVASCRIPT MODIFICATIONS
+
+
+*/
 (function()
 {
     function Start()
     {
+        
         leftSide();
+        
         rightSide();
+        
+        
 
     }
     window.addEventListener("load",Start);
@@ -12,30 +24,50 @@
     {
         let info=document.getElementsByClassName("info");
         
+        
         info[0].textContent="Veysel karani Boybay";
         info[1].textContent="Software Engineering Technology Student";
         info[2].textContent="Centennial College, since Jan 2020";
-
+        
+        
+        
 
     }
     function rightSide()
     {
         let Info=document.getElementsByTagName("th");
-
+        
+        
         Info[0].textContent="Contact Info";
         Info[1].textContent="Links";
+        
+        
 
         let Infolines=document.getElementsByTagName("td");
-
+        
         Infolines[0].textContent="(437)688****";
-        Infolines[1].textContent="LinkedIn";
         Infolines[2].textContent="vboybay@my.centennialcollege.ca";
-        Infolines[3].textContent="GitHub-Pages";
-        Infolines[4].textContent="";
+        
+        let links=document.getElementsByClassName("links");
+        
+        links[0].textContent="linkedIn";
+        links[1].textContent="GitHub Pages";
+        
+        let title=document.getElementById("contentTitle");
+        
+        title.textContent="A little bit about Veysel Boybay";
+        
+        let parags=document.getElementById("aboutme");
 
-
+        parags.textContent="My name is Veysel Boybay. I am Centennial College 1st year student and  studying Software Engineering Technology."+
+        "My short time goal is to succeed in my classes and learn as much as I can and find myself a co-op chance where I can improve myself more."+
+        "If I talk about my long term goal, because of the technological edvancements AI and ML pull my attention a lot."+
+        "I am feeling that working on these areas will exite me more. So, I will try to improve myself for these fields.";
+        
+        
     }
-
+    
+    
 
 
 })();
