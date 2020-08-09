b08b809ca1b88a9b0db902f8893a831188996617
veyselboybay <veysel.boybay@outlook.com>
2020-08-09 16:53:08 -0400

    Data from json injected to the page

--- Script/app.js
+++ Script/app.js
@@ -8,66 +8,16 @@
 */
 import {Data} from "./data.js"
 (function()
+
 {
+    
     function Start()
     {
         InitializePage();
-        leftSide();
         
-        rightSide();
-        
-        
-        
-
     }
     window.addEventListener("load",Start);
-
-    function leftSide()
-    {
-        let info=document.getElementsByClassName("info");
-        
-        
-        info[0].textContent="Veysel karani Boybay";
-        info[1].textContent="Software Engineering Technology Student";
-        info[2].textContent="Centennial College, since Jan 2020";
-        
-        
-        
-
-    }
-    function rightSide()
-    {
-        let Info=document.getElementsByTagName("th");
-        
-        
-        Info[0].textContent="Contact Info";
-        Info[1].textContent="Links";
-        
-        
-
-        let Infolines=document.getElementsByTagName("td");
-        
-        Infolines[0].textContent="(437)688****";
-        Infolines[2].textContent="vboybay@my.centennialcollege.ca";
-        
-        let links=document.getElementsByClassName("links");
-        
-        links[0].textContent="linkedIn";
-        links[1].textContent="GitHub Pages";
-        
-        let title=document.getElementById("contentTitle");
-        
-        title.textContent="A little bit about Veysel Boybay";
-        
-        let parags=document.getElementById("aboutme");
-
-        parags.textContent="My name is Veysel Boybay. I am Centennial College 1st year student and  studying Software Engineering Technology."+
-        "My short time goal is to succeed in my classes and learn as much as I can and find myself a co-op chance where I can improve myself more."+
-        "If I talk about my long term goal, because of the technological edvancements AI and ML pull my attention a lot."+
-        "I am feeling that working on these areas will exite me more. So, I will try to improve myself for these fields.";
-        
-        
-    }
+``
     function homeData()
     {
         let xhr=new XMLHttpRequest();
@@ -78,16 +28,23 @@ import {Data} from "./data.js"
             if((xhr.readyState===4)&&(xhr.status===200))
             {
                 let data=JSON.parse(xhr.responseText);
-                infoData=data.Info;
-                dataArray=[];
+                let infoData=data.info;
+                let dataArray=[];
                 for(const record of infoData)
                 {
                     let data=new Data();
                     data.setData(record);
                     dataArray.push(data);
                 }
-                console.log(dataArray);
+                
 
+                let pageData=document.getElementsByClassName("info");
+                pageData[0].innerHTML=dataArray[0].info1;
+                pageData[1].innerHTML=dataArray[0].info2;
+                pageData[2].innerHTML=dataArray[0].info3;
+                pageData[3].innerHTML=dataArray[0].info4;
+                pageData[4].innerHTML=dataArray[0].info5;
+                pageData[5].innerHTML=dataArray[0].info6;
             }
         });
     }
